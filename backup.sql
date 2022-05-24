52bc3a1e5b98619ad66400a9cddaadfeb7671b55
Laetitia DECRE <laetitia.decre@gmail.com>
2022-05-24 09:56:49 +0200

    deployement heroku

---
+++ backup.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+
+    id INT AUTO_INCREMENT ,
+    email TEXT ,
+    password TEXT ,
+    secret TEXT ,
+    creation_date DATETIME ,
+    bloked INT(11)
+
+);
--- src/connect.php
+++ src/connect.php
@@ -1,5 +1,15 @@
 <?php
 
-    $db = new PDO('mysql:host=localhost:3307;dbname=tarif-transports;charset=utf8', 'root', '');
+try {
+    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+    //$db = new PDO('mysql:host=localhost:3307;dbname=tarif-transports;charset=utf8', 'root', '');
 
 ?>

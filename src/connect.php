2b4c8e37c7a301d0c5e5daa8399a30abf37dc73a
Laetitia DECRE <laetitia.decre@gmail.com>
2022-05-25 10:30:08 +0200

    test pdo

--- src/connect.php
+++ src/connect.php
@@ -22,7 +22,7 @@ try {
     $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
     // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+   // echo "Connected successfully";
     }
 catch(PDOException $e)
     {
